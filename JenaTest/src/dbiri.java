0d90061e7ba2b443d451cdf226a1286fe288a30c
takuyamine <gp17a128@oecu.jp>
2022-01-14 17:09:11 +0900

    1/14更新

--- JenaTest/src/dbiri.java
+++ JenaTest/src/dbiri.java
@@ -1,24 +1,26 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class dbiri {
 	static public void main(String[] args) throws FileNotFoundException{
 
 		//入力ファイル指定
-				File file = new File("input/dbpediaen3.txt");
+				File file = new File("input/dbpediaen5.txt");
 				
 				//ファイルの読み込み用のReaderの設定
 				BufferedReader br = new BufferedReader(	new InputStreamReader(new FileInputStream(file)));
@@ -46,19 +48,29 @@ public class dbiri {
 			    ((QueryEngineHTTP)qexec).addParam("timeout", "100000") ;
 
 			    //出力用のファイルの作成
-			    FileOutputStream out;
-				out = new FileOutputStream("output/SPARQL-outiri.txt");
+			    //FileOutputStream out;
+				//out = new FileOutputStream("output/outiri1.txt");
 
 				// クエリの実行.
 			    ResultSet rs = qexec.execSelect();
-
+			    
+			    // FileWriterクラスのオブジェクトを生成する
+	            FileWriter file1 = new FileWriter("output/out-iri1.txt");
+	            // PrintWriterクラスのオブジェクトを生成する
+	            PrintWriter pw = new PrintWriter(new BufferedWriter(file1));
+			    while(rs.hasNext()) {
+			    	QuerySolution qs=rs.next();
+			    	System.out.println(qs);
+			    	pw.println(qs);
+			    }
+			    pw.close();
 			    // 結果の出力　※以下のどれか「１つ」を選ぶ（複数選ぶと，2つ目以降の結果が「空」になる）
-			 	ResultSetFormatter.out(System.out, rs, query);		//表形式で，標準出力に
-			 	//ResultSetFormatter.out(out, rs, query); 			//表形式で，ファイルに
+			 	//ResultSetFormatter.out(System.out, rs, query);		//表形式で，標準出力に
+			    //ResultSetFormatter.out(out, rs, query); 			//表形式で，ファイルに
 			 	//ResultSetFormatter.outputAsCSV(System.out, rs);	//CSV形式で，標準出力に
 			 	//ResultSetFormatter.outputAsCSV(out, rs);			//CSV形式で，ファイルに
-			 	
-			 	out.close();
+			 	qexec.close();
+			 	//out.close();
 			
 			} catch (Exception e) {
 			    e.printStackTrace();
--- JenaTest/src/dbiri2.java
+++ JenaTest/src/dbiri2.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import org.apache.jena.query.Query;
@@ -13,7 +14,6 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
@@ -21,12 +21,12 @@ public class dbiri2 {
 	static public void main(String[] args) throws FileNotFoundException{
 
 		//入力ファイル指定
-				File file = new File("input/dbpediaen5.txt");
-				
+				File file = new File("input/dbpediaen3.txt");
+
 				//ファイルの読み込み用のReaderの設定
 				BufferedReader br = new BufferedReader(	new InputStreamReader(new FileInputStream(file)));
-				
-				ArrayList<String> ListAll2=new ArrayList<String>();
+
+				ArrayList<String> Listo=new ArrayList<String>();
 		try {
 			while(br.ready()) {
 				String line = br.readLine(); //ファイルを1行ずつ読み込む
@@ -43,43 +43,67 @@ public class dbiri2 {
 					+ " } ";
 			//System.out.println(queryStr);
 			Query query = QueryFactory.create(queryStr);
-		    
+
 			QueryExecution qexec = QueryExecutionFactory.sparqlService("http://kg.hozo.jp/agraph/dedanpen/", query) ;
 		    ((QueryEngineHTTP)qexec).addParam("timeout", "100000") ;
 			ResultSet rs = qexec.execSelect();
 
-		    //ListAll2.add(rs.toString());
-		    //System.out.println(ListAll2);
+		   // Listo.add(rs.toString());
+		    //System.out.println(Listo);
 		    //ResultSet rs2 = ((QueryExecution) ListAll2).execSelect();
 			 // Remote execution.
+			while(rs.hasNext()){
+			QuerySolution qs = rs.next();
+			java.util.List<String> vars = rs.getResultVars();
+			for(int i=0;i<vars.size();i++) {
+				RDFNode node =qs.get(vars.get(i));
+
+				Listo.add(node.toString());
+				//System.out.println(Listo);
+
+				//FileOutputStream out;
+				/*out = new FileOutputStream("output/outiri2.csv");
+				ResultSetFormatter.out(out, (ResultSet) Listo);
+				out.close();*/
+			}
+			//System.out.println("test");
+			FileOutputStream out2 = new FileOutputStream("output/outiri3.txt");
+			OutputStreamWriter out2w = new OutputStreamWriter(out2);
 			
-			while(rs.hasNext()) {
-		    	QuerySolution qs=rs.next();
-		    	System.out.println(qs);
-		    	RDFNode node = qs.get("o");
-		    	System.out.println(node);
-		    }
+			
+			for(String uri: Listo) {
+
+				System.out.print("\t" + uri);
+				System.out.println();
+			    //out2w.write(line);
+				out2w.write(uri);
+			    out2w.write("\n");
+				//out2.flush();
+				
+			}//System.out.println();
+			out2w.close();
+			}
 
 			    //出力用のファイルの作成
-			    FileOutputStream out;
-				out = new FileOutputStream("output/outiri2.txt");
+			//FileOutputStream out;
+			//out = new FileOutputStream("output/outiri2.txt");
 
 				// クエリの実行.
-			
+
 			    // 結果の出力　※以下のどれか「１つ」を選ぶ（複数選ぶと，2つ目以降の結果が「空」になる）
 			 	//ResultSetFormatter.out(System.out, rs, query);		//表形式で，標準出力に
-			 	ResultSetFormatter.out(out, rs, query); 			//表形式で，ファイルに
+			 	//ResultSetFormatter.out(out, rs, query); 			//表形式で，ファイルに
 			 	//ResultSetFormatter.outputAsCSV(System.out, rs);	//CSV形式で，標準出力に
 			 	//ResultSetFormatter.outputAsCSV(out, rs);			//CSV形式で，ファイルに
-				
+
 			 	qexec.close();
-			 	out.close();
-			 
-			
-			
+			 	//out.close();
 
 
-			
+
+
+
+
 }	br.close();
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
